d5ea12d6f3c1ed9a64815d9e135a7f34f5821869
castagno <gstlp82@gmail.com>
2016-04-09 16:32:19 -0300

    Gradle config files

--- src/calculator/MainFile.java
+++ src/calculator/MainFile.java
@@ -1,10 +1,22 @@
 package calculator;
 
 public class MainFile {
+    public static final String MENSAJE_DE_ERROR_POR_FALTA_O_EXCESO_DE_ARGUMENTOS_AL_EJECUTAR = "Proveer dos parametros. Ejemplo: java -jar proyecto.jar <miNombre> <nombreDeOtraPersona>";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        if (args.length == 2) {
+            String miNombre = args[0];
+            String nombreDeOtraPersona = args[1];
+            String saludo = armarSaludo(miNombre, nombreDeOtraPersona);
+            System.out.print(saludo);
+        } else {
+            System.err.print(MENSAJE_DE_ERROR_POR_FALTA_O_EXCESO_DE_ARGUMENTOS_AL_EJECUTAR);
+        }
 
 	}
 
+
+    private static String armarSaludo(String miNombre, String nombreDeOtraPersona) {
+        return "Hola " + nombreDeOtraPersona + ", soy " + miNombre;
+    }
 }
